fff7ffbd598a1ed246c7ffbe3502982e8dfd5b18
Tiffany MANOLIS <tmanolis@e3r4p12.42.fr>
2021-06-02 12:07:45 +0200

    tester approuved

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tmanolis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 17:21:12 by tmanolis          #+#    #+#             */
-/*   Updated: 2021/05/27 12:36:44 by tmanolis         ###   ########.fr       */
+/*   Updated: 2021/06/02 12:06:25 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	j;
 	
 	i = 0;
-
-	if (!(needle))
+	if (*needle == '\0' || len == 0)
 		return ((char *)haystack);
 	while (i < len)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j])
+		while (haystack[i + j] == needle[j] && ((i + j) < len))
 		{
 			if (needle[j + 1] == '\0')
 				return ((char *)haystack + i);
